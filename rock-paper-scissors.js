d4fd6a8b11cd18025490236339056dd4e9f124d6
zakbw <zakb.whitehead@hotmail.com>
2022-11-26 20:37:37 +1100

    Main function complete

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let answers = ["rock", "paper", "scissors"] 
     const random = Math.floor(Math.random() * answers.length);
@@ -6,13 +9,37 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
     if(playerChoice === computerChoice) {
-        return ('tie')
-    } else {
-        return ('none')
-    }
+        return ('Both played the same. Tie.')
+            } else if(playerChoice == 'rock') {
+                if(computerChoice == 'paper')
+                    return ('Computer plays paper. You lose.')
+                if(computerChoice == 'scissors')
+                    return ('Computer plays scissors. You win!')
+                        let playerScore = playerScore++;
+            } else if(playerChoice == 'paper') {
+                if(computerChoice == 'scissors')
+                    return ('Computer plays scissors. You lose.') 
+                if(computerChoice == 'rock')
+                    return ('Computer plays rock. You win!')
+            } else if(playerChoice == 'scissors') {
+                if(computerChoice == 'rock')
+                    return ('Computer plays rock. You lose.')
+                if(computerChoice == 'paper')
+                    return ('Computer plays paper. You win!')
+            }
 }
 
-playRound()
+function game(){
+    playRound()
+        if(playerScore === 5)
+            console.log('You won 5 games. Congratulations!')
+        else if(computerScore === 5)
+            console.log('Computer won 5 games. You lose...')
+}
+
+console.log(playerScore)
+
+game()
 
 const playerChoice = prompt("Scissors, Paper or Rock?").toLowerCase();
 const computerChoice = getComputerChoice();
